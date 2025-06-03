60f5d6f93eee96e4ce2bbd5f5794a993825ca13d
bolohonov <bolohonov@list.ru>
2025-06-03 14:22:44 +0400

    add bankUI

---
+++ bankUI/src/main/java/org/example/bankui/BankUiApplication.java
@@ -0,0 +1,13 @@
+package org.example.bankui;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BankUiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BankUiApplication.class, args);
+    }
+
+}
