d7198c0652b8f93b11ada1306d0861957da28b09
bolohonov <bolohonov@list.ru>
2025-06-13 18:10:33 +0400

    add configService

--- configService/src/test/java/org/example/configservice/ConfigServiceApplicationTests.java
+++ configService/src/test/java/org/example/configservice/ConfigServiceApplicationTests.java
@@ -1,10 +1,10 @@
-package org.example.bank;
+package org.example.configservice;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class BankApplicationTests {
+class ConfigServiceApplicationTests {
 
     @Test
     void contextLoads() {
